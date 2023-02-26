3ff947b1532a9610c73d41fd0b6b89a877e30450
GyanPrakash2483 <gyanprakash2483@gmail.com>
2023-02-26 18:21:59 +0530

    Added file loading and API handling function

---
+++ server/api.js
@@ -0,0 +1,11 @@
+function handleAPIrequest(req, res) {
+
+	console.log('api');
+	return 'API response';
+
+}
+
+
+module.exports = {
+	handleAPIrequest
+}
--- server/main.js
+++ server/main.js
@@ -1,6 +1,28 @@
-http = require('http');
+const http = require('http');
+const fs = require('fs');
+const api = require('./api');
+const mime = require('mime')
+
+const client = {
+	path: __dirname + '/../client/'
+};
 
 http.createServer(function(req, res){
-	res.write('hello');
+	if(req.url == '/') {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(fs.readFileSync(client.path + '/index.html', 'utf-8', function(err, data){
+			res.write(toString(err));
+		}));
+	} else if(fs.existsSync(client.path + req.url)) {
+		res.writeHead(200, {'Content-Type': mime.getType(client.path + req.url)});
+		res.write(fs.readFileSync(client.path + req.url));
+	}else if(req.url == '/api') {
+		api.handleAPIrequest(req, res);
+
+	} else {
+		res.write('<h1> Error 404 </h1> <br> The page you are looking for could not be found.')
+	}
+	
 	res.end();
+
 }).listen(8080);
