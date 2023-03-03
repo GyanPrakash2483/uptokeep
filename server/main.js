276ef30cc579713840795bd8b27d2d3b05e526e7
GyanPrakash2483 <gyanprakash2483@gmail.com>
2023-03-03 11:44:34 +0530

    unfixed err stream_write_after_end

--- server/main.js
+++ server/main.js
@@ -8,21 +8,39 @@ const client = {
 };
 
 http.createServer(function(req, res){
-	if(req.url == '/') {
+	if(req.url == '/api') {
+
+		if(req.method == 'POST') {
+
+			req.on('data', function(data){
+				console.log(data); //yet to be tested
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				res.write('{"test": "working"}');
+			});
+			res.end();
+
+		}
+
+	}
+	else if(req.url == '/') {
+
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(fs.readFileSync(client.path + '/index.html', 'utf-8', function(err, data){
 			res.write(toString(err));
 		}));
+		res.end();
+
 	} else if(fs.existsSync(client.path + req.url)) {
+
 		res.writeHead(200, {'Content-Type': mime.getType(client.path + req.url)});
 		res.write(fs.readFileSync(client.path + req.url));
-	}else if(req.url == '/api') {
-		api.handleAPIrequest(req, res);
+		res.end();
 
 	} else {
-		res.write('<h1> Error 404 </h1> <br> The page you are looking for could not be found.')
+
+		res.write('<h1> Error 404 </h1> <br> The page you are looking for could not be found.');
+		res.end();
+
 	}
-	
-	res.end();
 
 }).listen(8080);
